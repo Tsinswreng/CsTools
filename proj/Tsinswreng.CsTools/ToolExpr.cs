32cf511babc56331cd7f13ee85fcbd514b9ea0b1
Tsinswreng <Tsinswreng@gmail.com>
2026-03-01 20:02:15 +0800

    緟構ToolExpr

--- proj/Tsinswreng.CsTools/ToolExpr.cs
+++ proj/Tsinswreng.CsTools/ToolExpr.cs
@@ -17,7 +17,7 @@ public static class ToolExpr {
 	/// <exception cref="ArgumentException">表达式不是有效的成员访问时抛出</exception>
 	public static string GetMemberName<T>(Expression<Func<T, obj?>> ExprMemb) {
 		if (ExprMemb == null) {
-			throw new ArgumentNullException(nameof(ExprMemb), "表达式不能为空");
+			throw new ArgumentNullException(nameof(ExprMemb), "Expression cannot be null");
 		}
 
 		// 核心逻辑：解析表达式树，处理装箱转换（值类型转object）的情况
@@ -31,14 +31,98 @@ public static class ToolExpr {
 		// 验证是否为成员访问表达式（属性/字段）
 		if (body is not MemberExpression memberExpr) {
 			throw new ArgumentException(
-				$"表达式 '{ExprMemb}' 不是有效的成员访问表达式（如 x=>x.属性/字段）",
+				$"Expression '{ExprMemb}' is not a valid member access expression (e.g., x=>x.Property/Field)",
 				nameof(ExprMemb));
 		}
 		// 返回成员名称（属性/字段名都适用）
 		return memberExpr.Member.Name;
 	}
 	
+	/// 尝试从单成员访问提取成员名称 x=>x.Id => ["Id"]
+	/// <returns>成功返回成员名称列表，失败返回null</returns>
+	public static IList<string>? TryGetMemberNamesFromSingleMember<T>(Expression<Func<T, obj?>> expr) {
+		Expression body = expr.Body;
+		
+		// 处理值类型装箱
+		if (body is UnaryExpression unary && unary.NodeType == ExpressionType.Convert && unary.Type == typeof(object)) {
+			body = unary.Operand;
+		}
+		
+		if (body is MemberExpression singleMember) {
+			return new List<string> { singleMember.Member.Name };
+		}
+		
+		return null;
+	}
+
+	/// 尝试从匿名对象初始化提取成员名称 x=>new{x.Id, x.Name} => ["Id", "Name"]
+	/// <returns>成功返回成员名称列表，失败返回null</returns>
+	public static IList<string>? TryGetMemberNamesFromNewExpr<T>(Expression<Func<T, obj?>> expr) {
+		Expression body = expr.Body;
+		
+		// 处理值类型装箱
+		if (body is UnaryExpression unary && unary.NodeType == ExpressionType.Convert && unary.Type == typeof(object)) {
+			body = unary.Operand;
+		}
+		
+		if (body is not NewExpression newExpr) {
+			return null;
+		}
+		
+		var names = new List<string>(newExpr.Arguments.Count);
+		foreach (var arg in newExpr.Arguments) {
+			Expression argExpr = arg;
+			// 处理参数内的装箱
+			if (argExpr is UnaryExpression argUnary && argUnary.NodeType == ExpressionType.Convert) {
+				argExpr = argUnary.Operand;
+			}
+			if (argExpr is MemberExpression memberExpr) {
+				names.Add(memberExpr.Member.Name);
+			}
+			else {
+				throw new ArgumentException(
+					$"Object initialization contains non-member access expression: '{arg}'",
+					nameof(expr));
+			}
+		}
+		return names;
+	}
+
+	/// 尝试从数组初始化提取成员名称 x=>new[]{x.Id, x.Name} => ["Id", "Name"]
+	/// <returns>成功返回成员名称列表，失败返回null</returns>
+	public static IList<string>? TryGetMemberNamesFromArrExpr<T>(Expression<Func<T, obj?>> expr) {
+		Expression body = expr.Body;
+		
+		// 处理值类型装箱
+		if (body is UnaryExpression unary && unary.NodeType == ExpressionType.Convert && unary.Type == typeof(object)) {
+			body = unary.Operand;
+		}
+		
+		if (body is not NewArrayExpression arrayExpr) {
+			return null;
+		}
+		
+		var names = new List<string>(arrayExpr.Expressions.Count);
+		foreach (var element in arrayExpr.Expressions) {
+			Expression elemExpr = element;
+			// 处理元素内的装箱
+			if (elemExpr is UnaryExpression elemUnary && elemUnary.NodeType == ExpressionType.Convert) {
+				elemExpr = elemUnary.Operand;
+			}
+			if (elemExpr is MemberExpression memberExpr) {
+				names.Add(memberExpr.Member.Name);
+			}
+			else {
+				throw new ArgumentException(
+					$"Array initialization contains non-member access expression: '{element}'",
+					nameof(expr));
+			}
+		}
+		return names;
+	}
+	
 	///  x=>new{x.Id, x.Name} => ["Id", "Name"]
+	/// x=>new[]{x.Id, x.Name} => ["Id", "Name"]
 	/// x=>x.Id => ["Id"]
 	/// 兼容AOT
 	public static IList<string> GetMemberNames<T>(Expression<Func<T, obj?>> ExprMembs) {
@@ -46,86 +130,19 @@ public static class ToolExpr {
 			throw new ArgumentNullException(nameof(ExprMembs));
 		}
 
-		Expression body = ExprMembs.Body;
+		// 尝试各种表达式类型
+		var result = TryGetMemberNamesFromSingleMember(ExprMembs) 
+			?? TryGetMemberNamesFromNewExpr(ExprMembs)
+			?? TryGetMemberNamesFromArrExpr(ExprMembs);
 
-		// 处理值类型装箱
-		if (body is UnaryExpression unary && unary.NodeType == ExpressionType.Convert && unary.Type == typeof(object)) {
-			body = unary.Operand;
-		}
-
-		// 情况1：单成员访问 x=>x.Id
-		if (body is MemberExpression singleMember) {
-			return new List<string> { singleMember.Member.Name };
-		}
-
-		// 情况2：匿名对象初始化 x=>new{x.Id, x.Name}
-		if (body is NewExpression newExpr) {
-			var names = new List<string>(newExpr.Arguments.Count);
-			foreach (var arg in newExpr.Arguments) {
-				Expression expr = arg;
-				// 处理参数内的装箱
-				if (expr is UnaryExpression argUnary && argUnary.NodeType == ExpressionType.Convert) {
-					expr = argUnary.Operand;
-				}
-				if (expr is MemberExpression memberExpr) {
-					names.Add(memberExpr.Member.Name);
-				}
-				else {
-					throw new ArgumentException(
-						$"匿名对象初始化中包含非成员访问表达式: '{arg}'",
-						nameof(ExprMembs));
-				}
-			}
-			return names;
+		if (result != null) {
+			return result;
 		}
 
 		throw new ArgumentException(
-			$"表达式 '{ExprMembs}' 必须是成员访问（x=>x.Prop）或匿名对象初始化（x=>new{{x.A, x.B}}）",
+			$"Expression '{ExprMembs}' must be a member access (x=>x.Prop), anonymous object initialization (x=>new{{x.A, x.B}}), or array initialization (x=>new[]{{x.A, x.B}})",
 			nameof(ExprMembs));
 	}
 	
-	///  x=>new{x.Id, x.Name} => ["Id", "Name"]
-	/// 兼容AOT
-	static IList<string> GetMemberNamesOnlySupportNewExpr<T>(Expression<Func<T, obj?>> ExprMembs) {
-		if (ExprMembs == null) {
-			throw new ArgumentNullException(nameof(ExprMembs), "表达式不能为空");
-		}
-
-		Expression body = ExprMembs.Body;
-
-		// 处理值类型装箱的情况
-		if (body is UnaryExpression unaryExpr && unaryExpr.NodeType == ExpressionType.Convert && unaryExpr.Type == typeof(object)) {
-			body = unaryExpr.Operand;
-		}
-
-		// 验证是否为匿名类型初始化表达式（NewExpression）
-		if (body is not NewExpression newExpr) {
-			throw new ArgumentException(
-				$"表达式 '{ExprMembs}' 不是有效的匿名对象初始化表达式（如 x=>new{{x.Id, x.Name}}）",
-				nameof(ExprMembs));
-		}
-
-		// 提取所有成员绑定
-		var memberNames = new List<string>();
-		
-		foreach (var arg in newExpr.Arguments) {
-			Expression memberAccess = arg;
-			
-			// 处理参数中的装箱转换
-			if (memberAccess is UnaryExpression argUnary && argUnary.NodeType == ExpressionType.Convert) {
-				memberAccess = argUnary.Operand;
-			}
-			
-			// 验证是否为成员访问
-			if (memberAccess is not MemberExpression memberExpr) {
-				throw new ArgumentException(
-					$"表达式包含非成员访问参数: '{arg}'",
-					nameof(ExprMembs));
-			}
-			
-			memberNames.Add(memberExpr.Member.Name);
-		}
-		return memberNames;
-	}
 }
 
